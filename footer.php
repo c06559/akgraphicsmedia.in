0921dc1289707691ced43748eab9e2bfaac48c4d
c06559 <c06559@gmail.com>
2026-02-05 10:37:57 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="back-to-top" onclick="window.scrollTo(0,0)"><i class="fa-solid fa-circle-chevron-up"></i></div>
+        <p>© 2026, Akgraphicsmedia.in. All rights reserved.</p>
+        <div><a href="blogs">Our Blogs</a> </div>
+    </footer>
