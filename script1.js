cb911217a6abbc001332532e5beb8be893126293
c06559 <c06559@gmail.com>
2026-02-19 16:08:09 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -42,3 +42,24 @@ document.querySelectorAll('.dropbtn').forEach(btn => {
 document.querySelector('.all-btn').addEventListener('click', function () {
     document.querySelector('.sub-header').classList.toggle('show');
 });
+
+
+
+// Disable   Contant
+document.addEventListener("contextmenu", event => event.preventDefault());
+
+document.onkeydown = function(e) {
+    if (e.keyCode == 123) { // F12
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+};
+
