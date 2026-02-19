2c26ca88622532d828d7a5f3f04529aab013b1d4
c06559 <c06559@gmail.com>
2026-02-19 16:07:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -61,3 +61,22 @@ document.querySelectorAll('.dropbtn').forEach(btn => {
 document.querySelector('.all-btn').addEventListener('click', function () {
     document.querySelector('.sub-header').classList.toggle('show');
 });
+
+<script>
+document.addEventListener("contextmenu", event => event.preventDefault());
+
+document.onkeydown = function(e) {
+    if (e.keyCode == 123) { // F12
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+};
+</script>
